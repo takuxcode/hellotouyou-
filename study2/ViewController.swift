a0044340c796e4857d5da2d8fde07d9425607d65
ogawa takuma <takuma@S-ogawa-takuma.local>
2017-07-30 15:23:26 +0900

    Initial Commit

---
+++ study2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  study2
+//
+//  Created by ogawa takuma on 2017/07/30.
+//  Copyright © 2017年 ogawa takuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
